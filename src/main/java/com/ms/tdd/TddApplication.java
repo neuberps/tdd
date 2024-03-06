2a9e9ec1cb6a57d52503ed9054ec749ee2111c85
Neuber Paiva de Souza <neuber.paiva@gmail.com>
2024-03-06 16:31:50 -0300

    Update TddApplication.java

--- src/main/java/com/ms/tdd/TddApplication.java
+++ src/main/java/com/ms/tdd/TddApplication.java
@@ -2,7 +2,9 @@ package com.ms.tdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.ms.tdd.repository"})
 @SpringBootApplication
 public class TddApplication {
 
