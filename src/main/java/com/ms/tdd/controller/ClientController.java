c73aff2b6a6ca2c55723e7a526c820c4da96af3d
Neuber Paiva de Souza <neuber.paiva@gmail.com>
2024-03-06 15:51:52 -0300

    Update ClientController.java

--- src/main/java/com/ms/tdd/controller/ClientController.java
+++ src/main/java/com/ms/tdd/controller/ClientController.java
@@ -1,29 +1,26 @@
 package com.ms.tdd.controller;
 
 import com.ms.tdd.model.Client;
-import com.ms.tdd.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
-    @Autowired
-    private ClientRepository clientRepository;
+    //@Autowired
+    //private ClientRepository clientRepository;
 
     @RequestMapping(value = "/clients")
     public List<Client> list() {
-        return clientRepository.findAll();
-        /*
+        //return clientRepository.findAll();
         return Arrays.asList(Client.builder().
                 name("Neuber")
                 .email("neuber.paiva@gmail.com")
                 .cel("9994545429").build());
 
-         */
     }
 
 }
