a6ec98d4511fe10fd7ccf44dd62b1fa0209379c6
Neuber Paiva de Souza <neuber.paiva@gmail.com>
2024-03-07 11:06:03 -0300

    update tests config

--- src/test/java/com/ms/tdd/TddApplicationTests.java
+++ src/test/java/com/ms/tdd/TddApplicationTests.java
@@ -1,10 +1,13 @@
 package com.ms.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class TddApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TddApplication.class)
+public class TddApplicationTests {
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/ms/tdd/controller/ClientControllerTests.java
+++ src/test/java/com/ms/tdd/controller/ClientControllerTests.java
@@ -1,23 +1,32 @@
 package com.ms.tdd.controller;
 
-import org.junit.jupiter.api.Test;
+import com.ms.tdd.TddApplicationTests;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest
-public class ClientControllerTests {
+public class ClientControllerTests extends TddApplicationTests {
+
+    private MockMvc mockMvc;
 
     @Autowired
-    private MockMvc mockMvc;
+    private ClientController controller;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
 
     @Test
     public void clientListTest() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/clients"))
             .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.jsonPath("$").isNotEmpty());
     }
 }
