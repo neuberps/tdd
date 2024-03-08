f46215f1effab0291f0d8c4554594c384eb9a618
Neuber Paiva de Souza <neuber.paiva@gmail.com>
2024-03-08 18:00:36 -0300

    update tests

--- src/main/java/com/ms/tdd/controller/ClientController.java
+++ src/main/java/com/ms/tdd/controller/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clients")
@@ -20,10 +21,25 @@ public class ClientController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Client> get(@PathVariable String id) {
+        return repository.findById(id);
+    }
+
     @PostMapping
     public Client create(@RequestBody Client entity) {
         entity.setId(ObjectId.get().toString());
         return repository.save(entity);
+    }
+
+    @PutMapping
+    public Client update(@RequestBody Client entity) {
+        return repository.save(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repository.deleteById(id);
     }
 
 }
--- src/test/java/com/ms/tdd/controller/ClientControllerTests.java
+++ src/test/java/com/ms/tdd/controller/ClientControllerTests.java
@@ -17,6 +17,8 @@ public class ClientControllerTests extends TddApplicationTests {
 
     private MockMvc mockMvc;
 
+    private String id;
+
     @Autowired
     private ClientController controller;
 
@@ -26,7 +28,7 @@ public class ClientControllerTests extends TddApplicationTests {
     }
 
     @Test
-    public void testClientList() throws Exception {
+    public void testList() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/clients"))
             .andDo(MockMvcResultHandlers.print())
             .andExpect(MockMvcResultMatchers.status().isOk())
@@ -34,11 +36,23 @@ public class ClientControllerTests extends TddApplicationTests {
     }
 
     @Test
-    public void testCreateClient() throws Exception
+    public void testCreate() throws Exception
     {
         this.mockMvc.perform( MockMvcRequestBuilders
                         .post("/clients")
                         .content(asJsonString(new Client(null, "Neuber", "neuber.paiva@gmail.com", "9994545429", "440120165656")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
+
+    @Test
+    public void testUpdate() throws Exception
+    {
+        this.mockMvc.perform( MockMvcRequestBuilders
+                        .put("/clients")
+                        .content(asJsonString(new Client("65eb0f8a2cc552132401c0ca", "Neuber Souza", "neuber.paiva@gmail.com", "00004545429", "5465420165656")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
